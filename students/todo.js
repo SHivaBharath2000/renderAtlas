4a6253db518a71053dda59270755d9065344eaa2
SHivaBharath2000 <152796403+SHivaBharath2000@users.noreply.github.com>
2024-07-29 23:57:44 +0530

    Add files via upload

---
+++ students/todo.js
@@ -0,0 +1,20 @@
+import express from 'express';
+ const todosRouter=express.Router();
+ let todos=[
+    { 
+        id:"1",
+        name:"Sleep",
+        isCompleted:false,
+    }
+ ]
+ todosRouter.get('/',(req,res)=>{
+    res.send(todos)
+ });
+ //delete using query parameter
+ todosRouter.delete("/",(req,res)=>{
+    const {id}=req.query;
+    todos=todos.filter((td)=>td.id!==id);
+     console.log("id",id)
+     res.send({msg:"Todo deleted successfully"})
+})
+export default todosRouter
